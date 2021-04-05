c935c3e1d4cbf7bc0af1fa17d3bd7d801bc7d66b
syldysnya <syldysnya@gmail.com>
2021-04-05 14:49:14 -0400

    Finish user.rb

--- skeleton/app/models/user.rb
+++ skeleton/app/models/user.rb
@@ -9,13 +9,24 @@
 #  created_at      :datetime         not null
 #  updated_at      :datetime         not null
 #
+# require 'bcrypt'
+
 class User < ApplicationRecord
+
+    attr_reader :password
+
     validates :user_name, presence: true, uniqueness: true
     validates :password_digest, presence: { message: 'Password can\'t be blank'}
     validates :session_token, presence: true, uniqueness: true
     validates :password, length: { minimum: 6, allow_nil: true }
 
     after_initialize :ensure_session_token
+
+    def self.find_by_credentials(user_name, password)
+        user = User.find_by(user_name: user_name)
+        return user if user && is_password?(password)
+
+    end
 
     def self.generate_session_token
         SecureRandom::urlsafe_base64(16)
@@ -27,5 +38,16 @@ class User < ApplicationRecord
         self.session_token
     end
 
-    
+    def ensure_session_token
+        self.session_token ||= User.generate_session_token
+    end
+
+    def password=(password)
+        @password = password
+        self.password_digest = BCrypt::Password.create(password)
+    end
+
+    def is_password?(password)
+        BCrypt::Password.new(self.password_digest).is_password?(password)
+    end
 end
